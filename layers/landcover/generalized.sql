820d4bfa3bb7dd49d30ffb2bd4fd6520391e99cb
Bjørnar Dervo <76587915+DervoDev@users.noreply.github.com>
2025-03-05 07:33:16 +0100

    landcover layer 9 fix
    layer 9 not generated, halted from possible union during selection. removed and try again.

--- layers/landcover/generalized.sql
+++ layers/landcover/generalized.sql
@@ -177,7 +177,7 @@ CREATE TABLE simplify_vw_z9 AS
     SELECT subclass,
            ST_MakeValid(
             ST_SnapToGrid(
-             ST_SimplifyVW(ST_Union(geometry), power(zres(9),2)),
+             ST_SimplifyVW(geometry, power(zres(9),2)),
              0.001)) AS geometry
     FROM simplify_vw_z10
     WHERE ST_Area(geometry) > power(zres(8),2)
